d92c327cd10836367dcd0fec1f4ea2d98bfc5f38
Martin Belev <martinbelev93@gmail.com>
2018-06-07 22:27:56 +0300

    #2 Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {AreasSelectorComponent} from './components/areas-selector/areas-selector.component';
-import {AreasMapSelectorComponent} from './components/areas-selector/areas-map-selector/areas-map-selector.component';
-import {AreasDropdownSelectorComponent} from './components/areas-selector/areas-dropdown-selector/areas-dropdown-selector.component';
+import { AreasSelectorComponent } from './components/areas-selector/areas-selector.component';
+import { AreasMapSelectorComponent } from './components/areas-selector/areas-map-selector/areas-map-selector.component';
+import { AreasDropdownSelectorComponent } from './components/areas-selector/areas-dropdown-selector/areas-dropdown-selector.component';
+import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -13,6 +15,10 @@ describe('AppComponent', () => {
         AreasMapSelectorComponent,
         AreasDropdownSelectorComponent
       ],
+      imports: [
+        FormsModule,
+        MultiselectDropdownModule
+      ]
     }).compileComponents();
   }));
 
