3f7128f772ba0faf29472a516b3f7f7afbd25976
Martin Belev <martinbelev93@gmail.com>
2018-06-10 22:59:14 +0300

    #10: Improve styling

--- src/app/components/areas-selector/areas-dropdown-selector/areas-dropdown-selector.component.ts
+++ src/app/components/areas-selector/areas-dropdown-selector/areas-dropdown-selector.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { IMultiSelectOption, IMultiSelectSettings } from 'angular-2-dropdown-multiselect';
+import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'angular-2-dropdown-multiselect';
 import { getValidHtmlId } from '../../../shared/id-util';
+import {TranslationService} from '../../../services/translation.service';
+import { LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'areas-dropdown-selector',
@@ -16,12 +18,30 @@ export class AreasDropdownSelectorComponent implements OnInit, OnChanges {
   @Output() private areaDeselected: EventEmitter<string> = new EventEmitter<string>();
 
   settings: IMultiSelectSettings;
+  textsSettings: IMultiSelectTexts;
   options: IMultiSelectOption[];
   selectedOptions: string[];
 
+  constructor(private translationService: TranslationService) {
+    translationService.languageChanged().subscribe((event: LangChangeEvent) => {
+      this.textsSettings = {
+        defaultTitle: event.translations['Select areas'],
+        checkAll: event.translations['Select all'],
+        uncheckAll: event.translations['Deselect all'],
+        checkedPlural: event.translations['selected'],
+        searchPlaceholder: event.translations['Search'],
+        searchEmptyResult: event.translations['Nothing found']
+      };
+    });
+  }
+
   ngOnInit() {
     this.settings = {
-      enableSearch: true
+      enableSearch: true,
+      showCheckAll: true,
+      showUncheckAll: true,
+      checkedStyle: 'fontawesome',
+      dynamicTitleMaxItems: 1
     };
   }
 
