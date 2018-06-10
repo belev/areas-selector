ea71636999c6c089307f2fbb3b12c51eac2d2a5c
Martin Belev <martinbelev93@gmail.com>
2018-06-10 23:09:15 +0300

    #10: Unsubscribe from language changed event

--- src/app/components/areas-selector/areas-dropdown-selector/areas-dropdown-selector.component.ts
+++ src/app/components/areas-selector/areas-dropdown-selector/areas-dropdown-selector.component.ts
@@ -1,8 +1,18 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'angular-2-dropdown-multiselect';
 import { getValidHtmlId } from '../../../shared/id-util';
 import {TranslationService} from '../../../services/translation.service';
 import { LangChangeEvent } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'areas-dropdown-selector',
@@ -10,12 +20,14 @@ import { LangChangeEvent } from '@ngx-translate/core';
   styleUrls: ['./areas-dropdown-selector.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AreasDropdownSelectorComponent implements OnInit, OnChanges {
+export class AreasDropdownSelectorComponent implements OnChanges, OnDestroy {
   @Input() private areas: {[key: string]: string} = {};
   @Input() private selectedAreas: string[] = [];
 
   @Output() private areaSelected: EventEmitter<string> = new EventEmitter<string>();
   @Output() private areaDeselected: EventEmitter<string> = new EventEmitter<string>();
+
+  private languageChangedSubscription: Subscription;
 
   settings: IMultiSelectSettings;
   textsSettings: IMultiSelectTexts;
@@ -23,7 +35,15 @@ export class AreasDropdownSelectorComponent implements OnInit, OnChanges {
   selectedOptions: string[];
 
   constructor(private translationService: TranslationService) {
-    translationService.languageChanged().subscribe((event: LangChangeEvent) => {
+    this.settings = {
+      enableSearch: true,
+      showCheckAll: true,
+      showUncheckAll: true,
+      checkedStyle: 'fontawesome',
+      dynamicTitleMaxItems: 1
+    };
+
+    this.languageChangedSubscription = translationService.languageChanged().subscribe((event: LangChangeEvent) => {
       this.textsSettings = {
         defaultTitle: event.translations['Select areas'],
         checkAll: event.translations['Select all'],
@@ -33,16 +53,6 @@ export class AreasDropdownSelectorComponent implements OnInit, OnChanges {
         searchEmptyResult: event.translations['Nothing found']
       };
     });
-  }
-
-  ngOnInit() {
-    this.settings = {
-      enableSearch: true,
-      showCheckAll: true,
-      showUncheckAll: true,
-      checkedStyle: 'fontawesome',
-      dynamicTitleMaxItems: 1
-    };
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -55,6 +65,10 @@ export class AreasDropdownSelectorComponent implements OnInit, OnChanges {
     if (changes.selectedAreas && changes.selectedAreas.currentValue) {
       this.selectedOptions = [...changes.selectedAreas.currentValue];
     }
+  }
+
+  ngOnDestroy() {
+    this.languageChangedSubscription.unsubscribe();
   }
 
   onAdded(value) {
